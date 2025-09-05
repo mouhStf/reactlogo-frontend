17d05e5f4c07a098701de35e313539492b549b32
mouhStf <mouhamadou.mstf@gmail.com>
2025-09-05 23:24:01 +0000

    add .env file support in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
 
-const API_BASE_URL = 'http://localhost:8080';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost';
 
 // Main App Component
 export default function App() {
